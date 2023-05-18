19458dd9effec8d54bd89dd27c7270ccd29d9ec2
NikhilBommawar <112411540+NikhilBommawar@users.noreply.github.com>
2023-05-18 16:37:01 +0530

    Frontend source files

---
+++ src/app/service/backend/backend.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient , HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, tap } from 'rxjs';
+// import { InputModel } from 'src/app/model/InputModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class BackendService {
+  
+  constructor(private http : HttpClient) { }
+
+  readonly httpOptions = {
+    headers: new HttpHeaders({ 
+      'Access-Control-Allow-Origin':'*',
+      'Authorization':'authkey',
+      'userid':'1'
+    })
+  };
+
+
+  baseUrl : string = 'http://localhost:8080/home/';
+
+  eval(stack : any) : any{
+    console.log("stack in backend eval", stack ) 
+    console.log(JSON.stringify(stack))
+    return  this.http.post(this.baseUrl + 'calc',JSON.stringify(stack));
+  }
+
+ 
+  }
+
+ 
+
+
+
+
